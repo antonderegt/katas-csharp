14d5652f1d9a4dcd99d81fa765456010dfc3b6ac
Anton de Regt <antonderegt@pm.me>
2021-05-19 10:24:00 +0200

    Add kata reducto

---
+++ Katas/Reducto/Reducto.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace Katas.Reducto
+{
+    /// <summary>
+    /// Responsible for Reducto Multiplictum.
+    /// </summary>
+    public class Reducto
+    {
+        /// <summary>
+        /// Sums up all input values and calculates the product of the digits of the sum until the result is a single digit.
+        /// </summary>
+        /// <param name="list">Unknown number of ints</param>
+        /// <returns>Single digit</returns>
+        public static int SumDigProd(params int[] list)
+        {
+            int sumOfInputs = list.Sum();
+            int productOfDigits = sumOfInputs;
+
+            // Take the product of the digits until the result is a single digit
+            while (productOfDigits > 10)
+            {
+                productOfDigits = ProductOfDigits(productOfDigits);
+            }
+
+            return productOfDigits;
+        }
+
+        /// <summary>
+        /// Calculates the product of the individual digits in a number.
+        /// </summary>
+        /// <param name="number">Number of any length</param>
+        /// <returns>Product of digits</returns>
+        public static int ProductOfDigits(int number)
+        {
+            return number.ToString().Aggregate(1, (total, c) => (c - '0') * total);
+        }
+    }
+}
---
+++ KatasTests/ReductoTests.cs
@@ -0,0 +1,42 @@
+﻿using Katas.Reducto;
+using Xunit;
+
+namespace KatasTests
+{
+    public class ReductoTests
+    {
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(9, 9)]
+        [InlineData(7, 9, 8)]
+        [InlineData(6, 16, 28)]
+        [InlineData(1, 111111111)]
+        [InlineData(2, 1, 2, 3, 4, 5, 6)]
+        [InlineData(6, 8, 16, 89, 3)]
+        [InlineData(6, 26, 497, 62, 841)]
+        [InlineData(6, 17737, 98723, 2)]
+        [InlineData(8, 123, -99)]
+        [InlineData(8, 167, 167, 167, 167, 167, 3)]
+        [InlineData(2, 98526, 54, 863, 156489, 45, 6156)]
+        public void SumDigProd_SingleAndMultipleNumbersAsInput_ShouldReturnOneDigit(int expected, params int[] inputValues)
+        {
+            // Arrange
+            // Act
+            int actual = Reducto.SumDigProd(inputValues);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ProductOfDigits_NumberWithTwoDigits_ShouldReturnProductOfDigits()
+        {
+            // Arrange
+            int input = 44;
+            int expected = 16;
+            // Act
+            int actual = Reducto.ProductOfDigits(input);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
