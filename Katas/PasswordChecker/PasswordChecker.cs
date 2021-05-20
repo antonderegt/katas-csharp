57302e4f5f14b0a9870477b57cc9d61841209a89
antonderegt <antonderegt@pm.me>
2021-05-20 10:22:24 +0200

    Add kata password checker

---
+++ Katas/PasswordChecker/PasswordChecker.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+
+namespace Katas.PasswordChecker
+{
+    public class PasswordChecker
+    {
+        private const int MinLength = 8;
+        private const string Strong = "Strong";
+        private const string Moderate = "Moderate";
+        private const string Weak = "Weak";
+        private const string Invalid = "Invalid";
+
+        public static bool ContainsLowercase(string password)
+        {
+            return password.Any(char.IsLower);
+        }
+
+        public static bool ContainsUppercase(string password)
+        {
+            return password.Any(char.IsUpper);
+        }
+
+        public static bool ContainsDigit(string password)
+        {
+            return password.Any(char.IsDigit);
+        }
+
+        public static bool ContainsSpecialChar(string password)
+        {
+            return password.Any(ch => !char.IsLetterOrDigit(ch));
+        }
+
+        public static bool ContainsWhiteSpace(string password)
+        {
+            return password.Any(ch => ch == ' ');
+        }
+
+        public static bool CheckLength(string password)
+        {
+            return password.Length >= MinLength;
+        }
+
+        public delegate bool CharChecker(string password);
+        public static bool CheckChar(string password, CharChecker charChecker)
+        {
+            return charChecker(password);
+        }
+
+        //int containsLowercase = ContainsLowercase(password) ? 1 : 0;
+        //int containsUppercase = ContainsUppercase(password) ? 1 : 0;
+        //int containsDigit = ContainsDigit(password) ? 1 : 0;
+        //int containsSpecialChar = ContainsSpecialChar(password) ? 1 : 0;
+        //int checkLength = CheckLength(password) ? 1 : 0;
+        //bool containsWhiteSpace = ContainsWhiteSpace(password);
+
+        public static string CheckPassword(string password)
+        {
+            bool containsWhiteSpace = password.Any(ch => ch == ' ');
+            if (password.Length < 6 || containsWhiteSpace)
+            {
+                return Invalid;
+            }
+
+            int containsLowercase = password.Any(char.IsLower) ? 1 : 0;
+            int containsUppercase = password.Any(char.IsUpper) ? 1 : 0;
+            int containsDigit = password.Any(char.IsDigit) ? 1 : 0;
+            int containsSpecialChar = password.Any(ch => !char.IsLetterOrDigit(ch)) ? 1 : 0;
+            int checkLength = password.Length >= MinLength ? 1 : 0;
+
+            ShowHints(containsLowercase, containsUppercase, containsDigit, containsSpecialChar, checkLength);
+
+            int passwordStrength = containsLowercase + containsUppercase + containsDigit + containsSpecialChar + checkLength;
+
+            if (passwordStrength == 5)
+                return Strong;
+
+            if (passwordStrength >= 3)
+                return Moderate;
+
+            return Weak;
+        }
+
+        private static void ShowHints(int containsLowercase, int containsUppercase, int containsDigit, int containsSpecialChar, int checkLength)
+        {
+            if (containsLowercase == 0)
+            {
+                Console.WriteLine("Password has to contain a lowercase letter.");
+            }
+            if (containsUppercase == 0)
+            {
+                Console.WriteLine("Password has to contain a uppercase letter.");
+            }
+            if (containsDigit == 0)
+            {
+                Console.WriteLine("Password has to contain a digit.");
+            }
+            if (containsSpecialChar == 0)
+            {
+                Console.WriteLine("Password has to contain a special char.");
+            }
+            if (checkLength == 0)
+            {
+                Console.WriteLine("Password has to be at least 8 characters.");
+            }
+        }
+    }
+}
---
+++ KatasTests/PasswordCheckerTests.cs
@@ -0,0 +1,98 @@
+﻿using Xunit;
+using Katas.PasswordChecker;
+
+namespace KatasTests
+{
+    public class PasswordCheckerTests
+    {
+        [Theory]
+        [InlineData(true, "a")]
+        [InlineData(false, "A")]
+        public void ContainsLowercase_PasswordIsEntered_ShouldReturnTrueIfContainsLowercase(bool expected, string password)
+        {
+            // Arrange
+            // Act
+            bool actual = PasswordChecker.ContainsLowercase(password);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(false, "a")]
+        [InlineData(true, "A")]
+        [InlineData(false, "!@!pass1")]
+        public void ContainsUppercase_PasswordIsEntered_ShouldReturnTrueIfContainsUppercase(bool expected, string password)
+        {
+            // Arrange
+            // Act
+            bool actual = PasswordChecker.ContainsUppercase(password);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(false, "a")]
+        [InlineData(true, "#")]
+        public void ContainsSpecialChar_PasswordIsEntered_ShouldReturnTrueIfContainsSpecialChar(bool expected, string password)
+        {
+            // Arrange
+            // Act
+            bool actual = PasswordChecker.ContainsSpecialChar(password);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(false, "a")]
+        [InlineData(true, "asdfgqwe")]
+        public void CheckLength_PasswordIsEntered_ShouldReturnTrueIfLengthIs8(bool expected, string password)
+        {
+            // Arrange
+            // Act
+            bool actual = PasswordChecker.CheckLength(password);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(false, "a")]
+        [InlineData(true, "1")]
+        public void ContainsDigit_PasswordIsEntered_ShouldReturnTrueIfContainsDigit(bool expected, string password)
+        {
+            // Arrange
+            // Act
+            bool actual = PasswordChecker.ContainsDigit(password);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(false, "a")]
+        [InlineData(true, "a a")]
+        public void ContainsWhiteSpace_PasswordIsEntered_ShouldReturnTrueIfContainsWhiteSpace(bool expected, string password)
+        {
+            // Arrange
+            // Act
+            bool actual = PasswordChecker.ContainsWhiteSpace(password);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Invalid", "stonk")]
+        [InlineData("Invalid", "pass word")]
+        [InlineData("Weak", "password")]
+        [InlineData("Weak", "11081992")]
+        [InlineData("Moderate", "mySecurePass123")]
+        [InlineData("Moderate", "!@!pass1")]
+        [InlineData("Strong", "@S3cur1t")]
+        public void CheckPassword_PasswordIsEntered_ShouldReturnTrueIfAllChecksPass(string expected, string password)
+        {
+            // Arrange
+            // Act
+            string actual = PasswordChecker.CheckPassword(password);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
