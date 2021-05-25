329e47a8a9ed45949b62c8e375cb860fc89eb5e6
antonderegt <antonderegt@pm.me>
2021-05-25 10:10:58 +0200

    Add kata Complete Word

---
+++ Katas/CompleteWord/CompleteWord.cs
@@ -0,0 +1,25 @@
+﻿namespace Katas.CompleteWord
+{
+    public class CompleteWord
+    {
+        public static bool CanComplete(string input, string word)
+        {
+            if (input.Equals(word)) return true;
+
+            for (int i = 0, j = 0; i < input.Length; i++, j++)
+            {
+                if (j >= word.Length)
+                    return false;
+
+                while (!input[i].Equals(word[j]))
+                {
+                    j++;
+                    if (j >= word.Length)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ KatasTests/CompleteWordTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Katas.CompleteWord;
+
+namespace KatasTests
+{
+    public class CompleteWordTests
+    {
+        [Theory]
+        [InlineData(true, "a", "a")]
+        [InlineData(false, "a", "b")]
+        [InlineData(true, "butl", "beautiful")]
+        [InlineData(false, "butlz", "beautiful")]
+        [InlineData(false, "tulb", "beautiful")]
+        [InlineData(false, "bbutl", "beautiful")]
+        [InlineData(true, "llo", "hello")]
+        public void CanComplete_WhenSuppliedWithInputAndWord_ReturnsIfInputCanBeCompletedToWord(bool expected, string input, string word)
+        {
+            // Arrange & Act
+            bool actual = CompleteWord.CanComplete(input, word);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
