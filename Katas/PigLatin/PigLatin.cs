9223b8b33a9ecfdf6c5c2bda77774ba6c0f4415f
antonderegt <antonderegt@pm.me>
2021-05-25 10:10:50 +0200

    Add kata Pig latin

---
+++ Katas/PigLatin/PigLatin.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace Katas.PigLatin
+{
+    public class PigLatin
+    {
+        public static string TranslateWord(string word)
+        {
+            if (word.Length == 0)
+                return "";
+
+            string specialChar = "";
+            if (!char.IsLetterOrDigit(word[^1]))
+            {
+                specialChar = word[^1].ToString();
+                word = word[0..^1];
+            }
+
+            bool startWithUppercase = false;
+            if (char.IsUpper(word[0]))
+            {
+                startWithUppercase = true;
+                word = word.ToLower();
+            }
+
+            char[] vowels = { 'a', 'i', 'e', 'o', 'u', 'A', 'I', 'E', 'O', 'U' };
+            bool startsWithVowel = vowels.Contains(word[0]);
+            int prefexIndex = -1;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (vowels.Contains(word[i]))
+                {
+                    prefexIndex = i;
+                    break;
+                }
+            }
+
+            string prefix = word.Substring(0, prefexIndex);
+            string postfix = startsWithVowel ? "yay" : "ay";
+            postfix += specialChar;
+            string middle;
+
+            if (startWithUppercase)
+            {
+                middle = word.Substring(prefexIndex, 1).ToUpper() + word[(prefexIndex + 1)..];
+            }
+            else
+            {
+                middle = word[prefexIndex..];
+            }
+
+            return middle + prefix + postfix;
+        }
+
+        public static string TranslateSentence(string sentence)
+        {
+            string pigLatinSentence = "";
+            string[] words = sentence.Split(' ');
+
+            foreach (string word in words)
+            {
+                pigLatinSentence += TranslateWord(word) + ' ';
+            }
+
+            return pigLatinSentence.Trim();
+        }
+    }
+}
---
+++ KatasTests/PigLatinTests.cs
@@ -0,0 +1,49 @@
+﻿using Katas.PigLatin;
+using System;
+using Xunit;
+
+namespace KatasTests
+{
+    public class PigLatinTests
+    {
+        [Theory]
+        [InlineData("flag", "agflay")]
+        [InlineData("Flag", "Agflay")]
+        [InlineData("Apple", "Appleyay")]
+        [InlineData("button", "uttonbay")]
+        [InlineData("", "")]
+        [InlineData("ate", "ateyay")]
+        [InlineData("apple", "appleyay")]
+        [InlineData("oaken", "oakenyay")]
+        [InlineData("eagle", "eagleyay")]
+        [InlineData("have", "avehay")]
+        [InlineData("cram", "amcray")]
+        [InlineData("take", "aketay")]
+        [InlineData("cat", "atcay")]
+        [InlineData("shrimp", "impshray")]
+        [InlineData("trebuchet", "ebuchettray")]
+        public void TranslateWord_WhenGivenAWord_ShouldReturnPigLatinizedWord(string word, string expected)
+        {
+            // Arrange
+
+            // Act
+            string actual = PigLatin.TranslateWord(word);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Honey waffles.", "Oneyhay afflesway.")]
+        [InlineData("I like to eat honey waffles.", "Iyay ikelay otay eatyay oneyhay afflesway.")]
+        [InlineData("Do you think it is going to rain today?", "Oday ouyay inkthay ityay isyay oinggay otay ainray odaytay?")]
+        public void TranslateSentence_WhenGivenASentence_ShouldReturnPigLatinizedSentence(string sentence, string expected)
+        {
+            // Arrange
+
+            // Act
+            string actual = PigLatin.TranslateSentence(sentence);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
