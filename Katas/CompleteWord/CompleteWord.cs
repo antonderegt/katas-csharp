740640f2598b94207ec209835f84eb823efc701b
antonderegt <antonderegt@pm.me>
2021-05-25 10:12:12 +0200

    Add input check

--- Katas/CompleteWord/CompleteWord.cs
+++ Katas/CompleteWord/CompleteWord.cs
@@ -5,6 +5,7 @@
         public static bool CanComplete(string input, string word)
         {
             if (input.Equals(word)) return true;
+            if (input.Length > word.Length) return false;
 
             for (int i = 0, j = 0; i < input.Length; i++, j++)
             {
