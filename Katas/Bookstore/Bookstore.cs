10375d664208f4a5069e7bad62be7acf2d114bbf
antonderegt <antonderegt@pm.me>
2021-05-26 11:36:17 +0200

    Add kata Bookstore

---
+++ Katas/Bookstore/Bookstore.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas.Bookstore
+{
+    public class Bookstore
+    {
+        public static double PriceRecursive(int[] books)
+        {
+            if (books.Length == 0) return 0;
+
+            List<int> bookList = new(books);
+            HashSet<int> uniqueBooks = new();
+            for (int i = 0; i < bookList.Count; i++)
+            {
+                int currentBook = bookList.ElementAt(i);
+                if (!uniqueBooks.Contains(currentBook))
+                {
+                    uniqueBooks.Add(currentBook);
+                    bookList.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            int numUniqueBooks = uniqueBooks.Count;
+            double discount = CalculateDiscount(numUniqueBooks);
+
+            return PriceRecursive(bookList.ToArray()) + 8 * numUniqueBooks * discount;
+        }
+
+        public static double Price(int[] books)
+        {
+            if (books.Length == 0) return 0;
+
+            List<int> bookList = new(books);
+            double price = 0.0;
+
+            while (bookList.Count > 0)
+            {
+                HashSet<int> uniqueBooks = new();
+                for (int i = 0; i < bookList.Count; i++)
+                {
+                    int currentBook = bookList.ElementAt(i);
+                    if (!uniqueBooks.Contains(currentBook))
+                    {
+                        uniqueBooks.Add(currentBook);
+                        bookList.RemoveAt(i);
+                        i--;
+                    }
+                }
+
+                int numUniqueBooks = uniqueBooks.Count;
+                double discount = CalculateDiscount(numUniqueBooks);
+
+                price += 8 * numUniqueBooks * discount;
+            }
+
+            return price;
+        }
+
+        private static double CalculateDiscount(int numOfBooks)
+        {
+            return numOfBooks switch
+            {
+                4 => 0.8,
+                3 => 0.9,
+                2 => 0.95,
+                _ => 1.0,
+            };
+        }
+    }
+}
---
+++ KatasTests/BookstoreTests.cs
@@ -0,0 +1,96 @@
+﻿using Katas.Bookstore;
+using Xunit;
+
+namespace KatasTests
+{
+    public class BookstoreTests
+    {
+        [Theory]
+        [InlineData(0, new int[] { })]
+        [InlineData(8, new int[] { 1 })]
+        [InlineData(8, new int[] { 2 })]
+        [InlineData(8, new int[] { 3 })]
+        [InlineData(8 * 3, new int[] { 1, 1, 1 })]
+        public void PriceRecursive_OneOrZeroBooks_ShouldReturn0Or8(double expected, int[] books)
+        {
+            // Arrange
+            // Act
+            double actual = Bookstore.PriceRecursive(books);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(8 * 2 * 0.95, new int[] { 0, 1 })]
+        [InlineData(8 * 3 * 0.9, new int[] { 0, 2, 3 })]
+        [InlineData(8 * 4 * 0.8, new int[] { 0, 1, 2, 3 })]
+        public void PriceRecursive_MiltipleDifferentBooks_ShouldReturnDiscountedPrice(double expected, int[] books)
+        {
+            // Arrange
+            // Act
+            double actual = Bookstore.PriceRecursive(books);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(8 + (8 * 2 * 0.95), new int[] { 0, 0, 1 })]
+        [InlineData(2 * (8 * 2 * 0.95), new int[] { 0, 0, 1, 1 })]
+        [InlineData((8 * 4 * 0.8) + (8 * 2 * 0.95), new int[] { 0, 0, 1, 2, 2, 3 })]
+        [InlineData((8 * 3 * 0.9) + (8 * 3 * 0.9) + (8 * 3 * 0.9), new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 2 })]
+        [InlineData((8 * 4 * 0.8) + (8 * 3 * 0.9) + (8 * 3 * 0.9), new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3 })]
+        [InlineData((8 * 4 * 0.8) + (8 * 4 * 0.8) + 8, new int[] { 0, 0, 1, 1, 2, 2, 3, 3, 3 })]
+        public void PriceRecursive_MiltipleDifferentAndDoubleBooks_ShouldReturnDiscountedPrice(double expected, int[] books)
+        {
+            // Arrange
+            // Act
+            double actual = Bookstore.PriceRecursive(books);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(0, new int[] { })]
+        [InlineData(8, new int[] { 1 })]
+        [InlineData(8, new int[] { 2 })]
+        [InlineData(8, new int[] { 3 })]
+        [InlineData(8 * 3, new int[] { 1, 1, 1 })]
+        public void Price_OneOrZeroBooks_ShouldReturn0Or8(double expected, int[] books)
+        {
+            // Arrange
+            // Act
+            double actual = Bookstore.Price(books);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(8 * 2 * 0.95, new int[] { 0, 1 })]
+        [InlineData(8 * 3 * 0.9, new int[] { 0, 2, 3 })]
+        [InlineData(8 * 4 * 0.8, new int[] { 0, 1, 2, 3 })]
+        public void Price_MiltipleDifferentBooks_ShouldReturnDiscountedPrice(double expected, int[] books)
+        {
+            // Arrange
+            // Act
+            double actual = Bookstore.Price(books);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(8 + (8 * 2 * 0.95), new int[] { 0, 0, 1 })]
+        [InlineData(2 * (8 * 2 * 0.95), new int[] { 0, 0, 1, 1 })]
+        [InlineData((8 * 4 * 0.8) + (8 * 2 * 0.95), new int[] { 0, 0, 1, 2, 2, 3 })]
+        [InlineData((8 * 3 * 0.9) + (8 * 3 * 0.9) + (8 * 3 * 0.9), new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 2 })]
+        [InlineData((8 * 4 * 0.8) + (8 * 3 * 0.9) + (8 * 3 * 0.9), new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 2, 3 })]
+        [InlineData((8 * 4 * 0.8) + (8 * 4 * 0.8) + 8, new int[] { 0, 0, 1, 1, 2, 2, 3, 3, 3 })]
+        public void Price_MiltipleDifferentAndDoubleBooks_ShouldReturnDiscountedPrice(double expected, int[] books)
+        {
+            // Arrange
+            // Act
+            double actual = Bookstore.Price(books);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
