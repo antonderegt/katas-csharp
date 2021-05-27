6380666d0a8d1f43472bd987902b8159719d6923
antonderegt <antonderegt@pm.me>
2021-05-27 10:17:43 +0200

    Add kata Parseltongue

---
+++ Katas/Parseltongue/Parseltongue.cs
@@ -0,0 +1,33 @@
+﻿namespace Katas.Parseltongue
+{
+    public class Parseltongue
+    {
+        public static bool IsParseltongue(string sentence)
+        {
+            char charToCheck = 's';
+            sentence = sentence.ToLower();
+            bool previousWasCharToCheck = false;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                char currentChar = sentence[i];
+                bool nextWillBeCharToCheck = i + 1 < sentence.Length && sentence[i + 1].Equals(charToCheck);
+
+                if (currentChar.Equals(charToCheck))
+                {
+                    if (previousWasCharToCheck || nextWillBeCharToCheck)
+                    {
+                        previousWasCharToCheck = true;
+                        continue;
+                    }
+
+                    return false;
+                }
+
+                previousWasCharToCheck = false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ KatasTests/ParseltongueTests.cs
@@ -0,0 +1,28 @@
+﻿using Katas.Parseltongue;
+using Xunit;
+
+namespace KatasTests
+{
+    public class ParseltongueTests
+    {
+        [Theory]
+        [InlineData(true, "abc def")]
+        [InlineData(false, "s")]
+        [InlineData(true, "ss")]
+        [InlineData(true, "Sshe ssselected to eat that apple.")]
+        [InlineData(false, "She ssselected to eat that apple.")]
+        [InlineData(true, "You ssseldom sssspeak sso boldly, ssso messmerizingly.")]
+        [InlineData(false, "Steve likes to eat pancakes")]
+        [InlineData(true, "Sssteve likess to eat pancakess")]
+        [InlineData(false, "Beatrice samples lemonade")]
+        [InlineData(true, "Beatrice enjoysss lemonade")]
+        public void IsParselTongue_WhenSuppliedWithAString_ReturnTrueIfItIsInParselTongue(bool expected, string sentence)
+        {
+            // Arrange
+            // Act
+            bool actual = Parseltongue.IsParseltongue(sentence);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
