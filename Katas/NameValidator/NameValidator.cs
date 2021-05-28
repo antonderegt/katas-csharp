e2e635045324048e2af9ec293e22158e9793b911
antonderegt <antonderegt@pm.me>
2021-05-28 10:17:33 +0200

    Add Name Validator kata

---
+++ Katas/NameValidator/NameValidator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Katas.NameValidator
+{
+    public class NameValidator
+    {
+        public static bool ValidName(string name)
+        {
+            List<string> terms = name.Trim().Split(' ').Where(t => !string.IsNullOrEmpty(t)).ToList();
+
+            if (terms.Count == 2)
+            {
+                return IsInitial(terms[0]) && IsWord(terms[1]);
+            }
+
+            if (terms.Count == 3)
+            {
+                bool allInitialsExpended = IsWord(terms[0]) && IsWord(terms[1]) && IsWord(terms[2]);
+                bool middleInitialExpended = IsWord(terms[0]) && IsInitial(terms[1]) && IsWord(terms[2]);
+                bool initialsNotExpended = IsInitial(terms[0]) && IsInitial(terms[1]) && IsWord(terms[2]);
+
+                return allInitialsExpended || middleInitialExpended || initialsNotExpended;
+            }
+
+            return false;
+        }
+
+        public static bool IsInitial(string term)
+        {
+            bool hasLengthOfTwo = term.Length == 2;
+            bool startsWithLetter = char.IsLetter(term[0]);
+            bool startsWithUpper = term[0] == char.ToUpper(term[0]);
+            bool endsWithDot = term[^1].Equals('.');
+
+            return hasLengthOfTwo && startsWithLetter && startsWithUpper && endsWithDot;
+        }
+
+        public static bool IsWord(string term)
+        {
+            bool moreThanTwoChars = term.Length > 2;
+            bool startWithUpperCase = term[0] == char.ToUpper(term[0]);
+            bool containsOnlyChars = term.All(c => char.IsLetter(c));
+
+            return moreThanTwoChars && startWithUpperCase && containsOnlyChars;
+        }
+    }
+}
---
+++ KatasTests/NameValidatorTests.cs
@@ -0,0 +1,62 @@
+﻿using Katas.NameValidator;
+using Xunit;
+
+namespace KatasTests
+{
+    public class NameValidatorTests
+    {
+        [Theory]
+        [InlineData(false, "H. G. W.")]
+        [InlineData(false, "H. G. W. Herbert")]
+        [InlineData(true, " H. Herbert ")]
+        [InlineData(true, "H.  Herbert")]
+        [InlineData(true, "H. Wells")] // 1
+        [InlineData(true, "H. G. Wells")] // 2
+        [InlineData(true, "Herbert G. Wells")] // 3
+        [InlineData(true, "Herbert George Wells")] // 4
+        [InlineData(false, "Herbert")] // 5 Description = "Name must be 2 or 3 words."
+        [InlineData(false, "Herbert W. G. Wells")] // 6 Description = "Name must be 2 or 3 words"
+        [InlineData(false, "h. Wells")] // 7 Description= "Incorrect Capitalization."
+        [InlineData(false, "herbert G. Wells")] // 8  Description = "Incorrect Capitalization."
+        [InlineData(false, "H Wells")] // 9  Description = "Initials must end with a dot."
+        [InlineData(false, "Herb. Wells")] // 10  Description = "Words cannot end with a dot."
+        [InlineData(false, "H. George Wells")] // 11  Description = "First name is initial but middle name is word."
+        [InlineData(false, "Herbert George W.")] // 12 Description = "Last name cannot be an initial."
+        public void ValidName_SuppliedWithAName_ReturnsWhetherNameIsValid(bool expected, string name)
+        {
+            // Arrange
+            // Act
+            bool actual = NameValidator.ValidName(name);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(false, "Wells")]
+        [InlineData(false, "H")]
+        [InlineData(true, "H.")]
+        [InlineData(false, "h.")]
+        public void IsInitial_SuppliedWithAName_ReturnsWhetherWordIsValid(bool expected, string name)
+        {
+            // Arrange
+            // Act
+            bool actual = NameValidator.IsInitial(name);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(true, "Wells")]
+        [InlineData(false, "H")]
+        [InlineData(false, "H.")]
+        [InlineData(false, "wells")]
+        public void IsWord_SuppliedWithAName_ReturnsWhetherWordIsValid(bool expected, string name)
+        {
+            // Arrange
+            // Act
+            bool actual = NameValidator.IsWord(name);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
