aa5eeb364ae1525c9c189268f79ff1b957c2bf65
antonderegt <antonderegt@pm.me>
2021-06-01 09:28:09 +0200

    Add rgb checker kata

---
+++ Katas/ColorUtil/ColorUtil.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Globalization;
+
+namespace Katas.ColorUtil
+{
+    public class ColorUtil
+    {
+        public static bool ValidRgbColor(string rgb)
+        {
+            int numOfTerms = 3;
+            int trimStart = 4;
+            if (rgb.Substring(0, 5).Equals("rgba("))
+            {
+                numOfTerms = 4;
+                trimStart = 5;
+            }
+
+            rgb = rgb.Substring(trimStart);
+            rgb = rgb.Substring(0, rgb.Length - 1);
+            var terms = rgb.Split(",");
+
+            if (terms.Length != numOfTerms)
+                return false;
+
+            for (int i = 0; i < terms.Length; i++)
+            {
+                terms[i] = terms[i].Trim();
+            }
+
+            int r, g, b;
+            double a;
+            try
+            {
+                r = Int32.Parse(terms[0]);
+                g = Int32.Parse(terms[1]);
+                b = Int32.Parse(terms[2]);
+                if (numOfTerms == 4)
+                {
+                    string alpha = 0 + terms[3];
+                    a = double.Parse(alpha, CultureInfo.InvariantCulture);
+                    if (a < 0 || a > 1)
+                        return false;
+                }
+                return NumBetween0And255(r) && NumBetween0And255(g) && NumBetween0And255(b);
+            }
+            catch
+            {
+                string percentageString = String.Join("", terms);
+                var newTerms = percentageString.Split("%");
+                try
+                {
+                    r = Int32.Parse(newTerms[0]);
+                    g = Int32.Parse(newTerms[1]);
+                    b = Int32.Parse(newTerms[2]);
+                    if (numOfTerms == 4)
+                    {
+                        string alpha = 0 + terms[3];
+                        a = Double.Parse(alpha);
+                        if (a < 0 || a > 1)
+                            return false;
+                    }
+                    return NumBetween0And100(r) && NumBetween0And100(g) && NumBetween0And100(b);
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static bool NumBetween0And255(int num)
+        {
+            if (num >= 0 && num <= 255)
+                return true;
+
+            return false;
+        }
+
+        public static bool NumBetween0And100(int num)
+        {
+            if (num >= 0 && num <= 100)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ KatasTests/ColorUtilTests.cs
@@ -0,0 +1,46 @@
+﻿using Katas.ColorUtil;
+using Xunit;
+
+namespace KatasTests
+{
+    public class ColorUtilTests
+    {
+        [Theory]
+        [InlineData(true, "rgb(0,0,0)")]
+        [InlineData(true, "rgb(255,255,255)")]
+        [InlineData(true, "rgb( 255 ,   255  ,255)")]
+        [InlineData(true, "rgb(0%,50%,100%)")]
+        [InlineData(false, "rgb(0,,0)")]
+        [InlineData(false, "rgb (0,0,0)")]
+        [InlineData(false, "rgb(0,0,0,0)")]
+        [InlineData(false, "rgb(-1,0,0)")]
+        [InlineData(false, "rgb(255,256,255)")]
+        [InlineData(false, "rgb(100%,100%,101%)")]
+        public static void ValidRgbColor_WhenSuppliedWithRgbString_ReturnsWhetherStringIsValid(bool expected, string rgb)
+        {
+            // Arrange
+            // Act
+            bool actual = ColorUtil.ValidRgbColor(rgb);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(true, "rgba(0,0,0,0)")]
+        [InlineData(true, "rgba(255,255,255,1)")]
+        [InlineData(true, "rgba(0,0,0,0.12345678)")]
+        [InlineData(true, "rgba(0,0,0,.8)")]
+        [InlineData(true, "rgba(0 , 127, 255 , 0.1)")]
+        [InlineData(false, "rgba(0,0,0)")]
+        [InlineData(false, "rgba(0,0,0,-1)")]
+        [InlineData(false, "rgba(0,0,0,1.1)")]
+        public static void ValidRgbColor_WhenSuppliedWithRgbaString_ReturnsWhetherStringIsValid(bool expected, string rgb)
+        {
+            // Arrange
+            // Act
+            bool actual = ColorUtil.ValidRgbColor(rgb);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
