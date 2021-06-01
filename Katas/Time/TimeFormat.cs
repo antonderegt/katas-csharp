48294e783a4b5d243816bd61cf0f97dad05e875f
antonderegt <antonderegt@pm.me>
2021-06-01 09:26:08 +0200

    Add Time kata

---
+++ Katas/Time/TimeFormat.cs
@@ -0,0 +1,25 @@
+﻿namespace Katas.Time
+{
+    public class TimeFormat
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            int hours = 0;
+            int minutes = 0;
+
+            if (seconds >= 60 * 60)
+            {
+                hours = seconds / (60 * 60);
+                seconds -= hours * 60 * 60;
+            }
+
+            if (seconds >= 60)
+            {
+                minutes = seconds / 60;
+                seconds -= minutes * 60;
+            }
+
+            return $"{hours:00}:{minutes:00}:{seconds:00}";
+        }
+    }
+}
---
+++ KatasTests/TimeFormatTests.cs
@@ -0,0 +1,23 @@
+﻿using Katas.Time;
+using Xunit;
+
+namespace KatasTests
+{
+    public class TimeFormatTests
+    {
+        [Theory]
+        [InlineData("00:00:00", 0)]
+        [InlineData("00:00:05", 5)]
+        [InlineData("00:01:00", 60)]
+        [InlineData("23:59:59", 86399)]
+        [InlineData("99:59:59", 359999)]
+        public void GetReadableTime_SuppliedWithSeconds_ReturnsInHHMMSSFormat(string expected, int seconds)
+        {
+            // Arrange
+            // Act
+            string actual = TimeFormat.GetReadableTime(seconds);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
