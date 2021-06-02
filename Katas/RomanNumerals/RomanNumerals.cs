37a714548ea0d88bcbdd6b7c40918c8fd9a40371
antonderegt <antonderegt@pm.me>
2021-06-02 10:22:38 +0200

    Add roman numerals converter kata

---
+++ Katas/RomanNumerals/RomanNumerals.cs
@@ -0,0 +1,98 @@
+﻿namespace Katas.RomanNumerals
+{
+    public class RomanNumerals
+    {
+        public static string ConvertToRoman(int num)
+        {
+            string roman = "";
+
+            roman += GetAllRomanOfType(num, 1000);
+            num -= (num / 1000) * 1000;
+
+            if (num >= 900)
+            {
+                roman += "CM";
+                num -= 900;
+            }
+            roman += GetAllRomanOfType(num, 500);
+            num -= (num / 500) * 500;
+
+            if (num >= 400)
+            {
+                roman += "CD";
+                num -= 400;
+            }
+            roman += GetAllRomanOfType(num, 100);
+            num -= (num / 100) * 100;
+
+            if (num >= 90)
+            {
+                roman += "XC";
+                num -= 90;
+            }
+            roman += GetAllRomanOfType(num, 50);
+            num -= (num / 50) * 50;
+
+            if (num >= 40)
+            {
+                roman += "XL";
+                num -= 40;
+            }
+            roman += GetAllRomanOfType(num, 10);
+            num -= (num / 10) * 10;
+
+            if(num == 9)
+            {
+                roman += "IX";
+                num = 0;
+            }
+            roman += GetAllRomanOfType(num, 5);
+            num -= (num / 5) * 5;
+
+            if (num == 4)
+            {
+                roman += "IV";
+                num = 0;
+            }
+            roman += GetAllRomanOfType(num, 1);
+
+            return roman;
+        }
+
+        public static string GetAllRomanOfType(int num, int divider)
+        {
+            string roman = "";
+            int d = num / divider;
+
+            while (d > 0)
+            {
+                roman += GetSingleRoman(divider);
+                d--;
+            }
+
+            return roman;
+        }
+
+        public static string GetSingleRoman(int num)
+        {
+            switch (num)
+            {
+                default:
+                case 1:
+                    return "I";
+                case 5:
+                    return "V";
+                case 10:
+                    return "X";
+                case 50:
+                    return "L";
+                case 100:
+                    return "C";
+                case 500:
+                    return "D";
+                case 1000:
+                    return "M";
+            }
+        }
+    }
+}
---
+++ KatasTests/RomanNumeralsTests.cs
@@ -0,0 +1,36 @@
+﻿using Katas.RomanNumerals;
+using Xunit;
+
+namespace KatasTests
+{
+    public class RomanNumeralsTests
+    {
+        [Theory]
+        [InlineData("I", 1)]
+        [InlineData("V", 5)]
+        [InlineData("X", 10)]
+        [InlineData("L", 50)]
+        [InlineData("C", 100)]
+        [InlineData("D", 500)]
+        [InlineData("M", 1000)]
+        [InlineData("MM", 2000)]
+        [InlineData("MMD", 2500)]
+        [InlineData("MMDXI", 2511)]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        [InlineData("XLV", 45)]
+        [InlineData("XCV", 95)]
+        [InlineData("CDLV", 455)]
+        [InlineData("CMXCVI", 996)]
+        [InlineData("MMMMCMXCVI", 4996)]
+        public void ConvertToRoman_WhenSuppliedWithNumber_ReturnsNumberConvertedToRoman(string expected, int num)
+        {
+            // Arrange
+            // Act
+            string actual = RomanNumerals.ConvertToRoman(num);
+            // Assert
+            Assert.True(expected.Equals(actual));
+        }
+    }
+}
