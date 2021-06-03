f286e8afe59867047ced0636b01a5bcf06463341
antonderegt <antonderegt@pm.me>
2021-06-03 10:24:51 +0200

    Add kata

---
+++ Katas/CreditCard/CreditCard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.CreditCard
+{
+    public class CreditCard
+    {
+        public static bool ValidateNumber(double num)
+        {
+            double numLength = Math.Floor(Math.Log10(num) + 1);
+            if (numLength < 14 || numLength > 19)
+                return false;
+
+            double checkDigit = num % 10;
+            double numMinusLastDigit = Math.Floor(num / 10);
+
+            var digitList = numMinusLastDigit.ToString().Reverse().Select(c => c - '0').ToList();
+            
+            for (int i = 0; i < digitList.Count; i += 2)
+            {
+                int doubled = digitList[i] * 2;
+                int doubledSingleDigit = doubled >= 10 ? 1 + doubled % 10 : doubled;
+                digitList[i] = doubledSingleDigit;
+            }
+
+            int sum = digitList.Sum();
+
+            return checkDigit == (10 - sum % 10);
+        }
+    }
+}
---
+++ KatasTests/CreditCardTests.cs
@@ -0,0 +1,28 @@
+﻿using Katas.CreditCard;
+using Xunit;
+
+namespace KatasTests
+{
+    public class CreditCardTests
+    {
+        [Theory]
+        [InlineData(false, 123)]
+        [InlineData(false, 79927398714)]
+        [InlineData(false, 79927398713)]
+        [InlineData(true, 709092739800713)]
+        [InlineData(false, 1234567890123456)]
+        [InlineData(true, 12345678901237)]
+        [InlineData(true, 5496683867445267)]
+        [InlineData(false, 4508793361140566)]
+        [InlineData(true, 376785877526048)]
+        [InlineData(false, 36717601781975)]
+        public void ValidateNumber_WhenSuppliedWithNumber_ReturnsTrueIfNumberIsValidOtherwiseFalse(bool expected, double num)
+        {
+            // Arrange
+            // Act
+            bool actual = CreditCard.ValidateNumber(num);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
