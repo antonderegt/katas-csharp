7eebb01996a2b7094b52a6689692c57861a40be9
antonderegt <antonderegt@pm.me>
2021-06-04 11:47:49 +0200

    Add kata freddy fridays

---
+++ Katas/FreddyFridays/FreddyFridays.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Katas.FreddyFridays
+{
+    public class FreddyFridays
+    {
+        public static int FindFridaysThe13thInYear(int year)
+        {
+            DateTime date = new(year, 1, 13);
+            DayOfWeek friday = DayOfWeek.Friday;
+            int friday13 = 0;
+
+            while(date.Year == year)
+            {
+                if(date.DayOfWeek == friday)
+                    friday13++;
+
+                date = date.AddMonths(1);
+            }
+
+            return friday13;
+        }
+    }
+}
---
+++ KatasTests/FreddyFridaysTests.cs
@@ -0,0 +1,23 @@
+﻿using Katas.FreddyFridays;
+using Xunit;
+
+namespace KatasTests
+{
+    public class FreddyFridaysTests
+    {
+        [Theory]
+        [InlineData(2, 2020)]
+        [InlineData(1, 2016)]
+        [InlineData(3, 2015)]
+        [InlineData(2, 2023)]
+        [InlineData(2, 2024)]
+        public void FindFridaysThe13thInYear_WhenSuppliedWithAYear_ReturnsTheNumberOfFriday13ths(int expected, int year)
+        {
+            // Arrange
+            // Act
+            int actual = FreddyFridays.FindFridaysThe13thInYear(year);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
